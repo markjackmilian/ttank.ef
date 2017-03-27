1f25d7edd90c992122d93cb0ae0c5252593274b2
Marco Milani <emme.milani@hotmail.it>
2017-03-27 16:18:29 +0200

    Add per class inheritance

---
+++ ttanks.ef/ttanks.inheritance_class/Order.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ttanks.inheritance_class
+{
+    [Table("Order")]
+    public partial class Order
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Order()
+        {
+            OrderItem = new HashSet<OrderItem>();
+        }
+
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        [StringLength(10)]
+        public string OrderNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public decimal? TotalAmount { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderItem> OrderItem { get; set; }
+    }
+}
