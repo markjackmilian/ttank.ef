0dd4fbebe35cfe42a764674d3df76f9e4ef7f55a
Marco Milani <emme.milani@hotmail.it>
2017-03-27 12:44:54 +0200

    add mdf

---
+++ ttanks.ef/ttanks.ef.mdf_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ttanks.ef.mdf_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Model1Container())
+            {
+                context.StudentSet.Add(new Student
+                {
+                    Name = "pippo"
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
