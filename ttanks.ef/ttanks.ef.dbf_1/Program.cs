b82c969f2c3bb083189ee116716d70b008f50d82
Marco Milani <emme.milani@hotmail.it>
2017-03-27 12:35:22 +0200

    Add example DF

---
+++ ttanks.ef/ttanks.ef.dbf_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ttanks.ef.dbf_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new exampleEntities())
+            {
+                var allCustomer = context.Customer.ToList();
+
+                allCustomer.ForEach(f =>
+                {
+                    Console.WriteLine($"{f.FirstName} {f.LastName}");
+                });
+
+                Console.WriteLine($"There are {allCustomer.Count} customers!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
